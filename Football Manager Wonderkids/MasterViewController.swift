b0664f9162b0ffd3ba97fb7647bade090c0cbb33
izzy <hello@izzyali.co.uk>
2018-02-22 09:04:58 +0000

    initial commit with remote connected

--- Football Manager Wonderkids/MasterViewController.swift
+++ Football Manager Wonderkids/MasterViewController.swift
@@ -25,6 +25,7 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
