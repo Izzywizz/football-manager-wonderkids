d1d593d66f29f1541f33e634163b27fa3bc172e6
izzy <hello@izzyali.co.uk>
2018-02-22 09:01:32 +0000

    Initial Commit

---
+++ Football Manager WonderkidsTests/Football_Manager_WonderkidsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Football_Manager_WonderkidsTests.swift
+//  Football Manager WonderkidsTests
+//
+//  Created by Izzy Ali on 22/02/2018.
+//  Copyright © 2018 Izzy Ali. All rights reserved.
+//
+
+import XCTest
+@testable import Football_Manager_Wonderkids
+
+class Football_Manager_WonderkidsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
