e666549d5575c9d00bfcc57ae917b682037f4043
izzy <hello@izzyali.co.uk>
2018-03-01 17:22:51 +0000

    created basic view for the player

--- Football Manager Wonderkids/Cells/PlayerTableViewCell.swift
+++ Football Manager Wonderkids/Cells/PlayerTableViewCell.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class PlayerTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var ratingView: UIView!
     @IBOutlet weak var playerName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        ViewHelper.roundedCorners(for: [ratingView], withRadius: 17.0)//this radius value will need to be changed if the view sizes change, divide the view width/height by 2 then p[lace this number here
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Football Manager Wonderkids/Helper/ViewHelper.swift
+++ Football Manager Wonderkids/Helper/ViewHelper.swift
@@ -13,7 +13,7 @@ struct ViewHelper {
      This method makes sure that the views have a nice round corner to it, mostly used for TextViews in order to match the designs.
      - parameter views: an array of subclasses of UIView which would include most views we want to be rounded, ie TextView/ Labels/ TextField
      */
-    static func roundedCorners(for views: [UIView], with cornerRadiusValue: CGFloat = 5.0) {
+    static func roundedCorners(for views: [UIView], withRadius cornerRadiusValue: CGFloat = 5.0) {
         for view in views {
             view.layer.cornerRadius = cornerRadiusValue
         }
